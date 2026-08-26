05d7ce95a78306d645593e9438fdb16c7a4adffa
sorowagniguizelika <sorozelika@gmail.com>
2026-08-26 16:29:50 +0000

    image ajoutée mais problème d'affichage dans l'émulateur

--- app/src/main/java/com/example/limonade/MainActivity.kt
+++ app/src/main/java/com/example/limonade/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
@@ -20,9 +23,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -69,12 +74,20 @@ fun LimonadeApp(modifier: Modifier = Modifier) {
         else -> R.string.Tap_the_empty_glass_to_start_again
     }
     // TITRE FIXE
-    Text(
-        text = stringResource(R.string.lemonade),
-        fontSize = 28.sp,
-        fontWeight = FontWeight.Bold,
-        modifier = Modifier.padding(top = 24.dp, bottom = 16.dp)
-    )
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.Yellow)
+            .padding(vertical = 16.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(R.string.lemonade),
+            fontSize = 28.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+    }
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
